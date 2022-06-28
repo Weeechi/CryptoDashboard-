116059357a74c0d84c15ef43f653a99b83046ac5
Weechi <Weechi@MacBook-Pro.local>
2022-06-28 12:57:14 -0400

    day1

--- crypto-dashboard/src/App.js
+++ crypto-dashboard/src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+
+
+
+
+
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
+
+
+useEffect(() =>{
+  axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+  .then(res => {
+    setCoins(res.data)
+    console.log(res.data);
+  }).catch(error => console.log(error))
+}, [])
+
   return (
     <div className="App">
      <h1>Api</h1>
